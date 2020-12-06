771a4dd3830888a6b8e4df78b695503dfce0187e
Taras Trush <tarastaras144@gmail.com>
2020-12-06 20:02:59 +0200

    Initial commit

---
+++ AlgorithmsTask/AlgorithmsTask/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsTask
+{
+    class Program
+    {
+        static int GetStepInOuterRail(int AmountOfRails)
+        {
+            if (AmountOfRails >= 2)
+                return (2 * AmountOfRails - 2);
+            else
+                return 0;
+        }
+
+        static int GetFirstStepInInnerRail(int AmountOfRails, int RailNumber)
+        {
+            if (AmountOfRails >= 2 && RailNumber < AmountOfRails)
+                return (2 * AmountOfRails - 2 * RailNumber);
+            else
+                return 0;
+        }
+
+        static int GetSumOfFirstAndSecondInnerSteps(int AmountOfRails)
+        {
+            return (2 * AmountOfRails - 2);
+        }
+
+        static string EncodeMethod(int AmountOfRails, string LineToEncode)
+        {
+            if (AmountOfRails < 2)
+                return null;
+            if (LineToEncode.Length == 0)
+                return LineToEncode;
+
+            int StepInOuterRail = GetStepInOuterRail(AmountOfRails);
+            int SumOfFirstAndSecondInnerSteps = GetSumOfFirstAndSecondInnerSteps(AmountOfRails);
+            string [] EncodedLine = new string[LineToEncode.Length];
+            int StepPosition = 0;
+            int PositionInLineToEncode = 0;
+            int PositionInEncodedLine = 0;
+            int Step = 0;
+            bool IsInnerRail = false;
+            
+            for (int RailCounter = 1; RailCounter <= AmountOfRails; RailCounter++)
+            {
+                if (RailCounter == 1 || RailCounter == AmountOfRails)
+                    Step = StepInOuterRail;
+                else
+                {
+                    Step = GetFirstStepInInnerRail(AmountOfRails, RailCounter);
+                    IsInnerRail = true;
+                }
+
+                StepPosition = PositionInLineToEncode;
+
+                while (StepPosition < LineToEncode.Length)
+                {
+                    EncodedLine[PositionInEncodedLine] = LineToEncode[StepPosition].ToString();
+                    PositionInEncodedLine++;
+                    StepPosition = StepPosition + Step;
+
+                    if (IsInnerRail)      
+                        Step = SumOfFirstAndSecondInnerSteps - Step;                                      
+                }
+
+                PositionInLineToEncode++;
+                IsInnerRail = false;
+            }
+            
+            return string.Join("",EncodedLine);
+        }
+
+        static string DecodeMethod(int AmountOfRails, string EncodedLine)
+        {
+            if (AmountOfRails < 2)
+                return null;
+            if (EncodedLine.Length == 0)
+                return EncodedLine;
+
+            int StepInOuterRail = GetStepInOuterRail(AmountOfRails);
+            int SumOfFirstAndSecondInnerSteps = GetSumOfFirstAndSecondInnerSteps(AmountOfRails);
+            string[] DecodedLine = new string[EncodedLine.Length];
+            int StepPosition = 0;
+            int PositionInDecodedLine = 0;
+            int PositionInEncodedLine = 0;
+            int Step = 0;
+            bool IsInnerRail = false;
+
+            for (int RailCounter = 1; RailCounter <= AmountOfRails; RailCounter++)
+            {
+                if (RailCounter == 1 || RailCounter == AmountOfRails)
+                    Step = StepInOuterRail;
+                else
+                {
+                    Step = GetFirstStepInInnerRail(AmountOfRails, RailCounter);
+                    IsInnerRail = true;
+                }
+
+                StepPosition = PositionInDecodedLine;
+
+                while (StepPosition < DecodedLine.Length)
+                {
+                    DecodedLine[StepPosition] = EncodedLine[PositionInEncodedLine].ToString();
+
+                    PositionInEncodedLine++;
+                    StepPosition = StepPosition + Step;
+
+                    if (IsInnerRail)
+                        Step = SumOfFirstAndSecondInnerSteps - Step;
+                }
+
+                PositionInDecodedLine++;
+                IsInnerRail = false;
+            }
+
+            return string.Join("", DecodedLine);
+        }
+
+        static void Main(string[] args)
+        {
+            string TestLine = "WEAREDISCOVEREDFLEEATONCE";
+
+            Console.WriteLine( EncodeMethod(3, TestLine));
+            Console.WriteLine(DecodeMethod(3, "WECRLTEERDSOEEFEAOCAIVDEN"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOPTask/OOPTask/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPTask
+{
+    abstract class Shape : IComparable<Shape>
+    {
+        protected double square;
+        public int CompareTo(Shape shape)
+        {
+            if (shape == null)
+            {
+                return 1;
+            }
+
+            return Comparer<double>.Default.Compare(this.square, shape.square);
+        }
+
+        public abstract double GetSquare();
+
+        public override string ToString()
+        {
+            return this.GetType().Name + " -> " + square.ToString();
+        }
+    }
+
+    class Square : Shape
+    {
+        double _side;
+        public Square(double side) 
+        {
+            _side = side;
+           square = GetSquare();
+        }
+
+       
+        public override double GetSquare()
+        {
+            return _side * _side;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        double _width;
+        double _height;
+
+        public Rectangle(double width, double height)
+        {
+            _width = width;
+            _height = height;
+
+            square = GetSquare();
+        }
+
+        public override double GetSquare()
+        {
+            return _width * _height;
+        }
+    }
+
+    class Triangle : Shape
+    {
+        double _base;
+        double _height;
+
+        public Triangle(double base_, double height)
+        {
+            _base = base_;
+            _height = height;
+
+            square = GetSquare();
+        }
+
+        public override double GetSquare()
+        {
+            return _base * _height;
+        }
+    }
+
+    class Circle : Shape
+    {
+        double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+
+            square = GetSquare();
+        }
+
+        public override double GetSquare()
+        {
+            return _radius * Math.PI;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var side = 3D;
+            var width = 10D;
+            var height = 4D;
+            var base_ = 3D;
+            var radius = 5D;
+            var shapes = new List<Shape>{ new Square(side),
+                                        new Rectangle(width, height),
+                                        new Triangle(base_, height),
+                                        new Circle(radius)};
+
+            Console.Write(String.Join(" ", shapes));
+
+            Console.WriteLine();
+
+            shapes.Sort();
+
+            Console.Write(String.Join(" ", shapes));
+
+            Console.ReadKey();      
+        }
+    }
+}
